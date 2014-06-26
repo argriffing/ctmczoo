1487fa6b5315619efd65c366fe2e8f9c94401f76
alex <argriffi@ncsu.edu>
2014-06-26 14:48:34 -0400

    add util module

---
+++ util.py
@@ -0,0 +1,18 @@
+"""
+AlgoPy boilerplate and other miscellaneous helper functions.
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import algopy
+
+
+def eval_grad(f, theta):
+    theta = algopy.UTPM.init_jacobian(theta)
+    return algopy.UTPM.extract_jacobian(f(theta))
+
+
+def eval_hess(f, theta):
+    theta = algopy.UTPM.init_hessian(theta)
+    return algopy.UTPM.extract_hessian(len(theta), f(theta))
+
